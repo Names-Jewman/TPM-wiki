52cacc9c2d4b79ae108609fa06618b869ef4acf9
James Newman <namesjewman@gmail.com>
2018-08-31 12:08:32 -0600

    initial commit

---
+++ includes/leftMenu.php
@@ -0,0 +1,15 @@
+<div id="leftBar">
+    <div id="OCLogo"></div>
+    <div id="wikiTitle">TPM<span>WIKI</span></div>
+    <div class="listTitle">The Pillars</div>
+    <ul id="leftMenu">
+        <li><a href="">Focused Improvement</a></li>
+        <li><a href="">Autonomous Maintenance</a></li>
+        <li><a href="">Planned Maintenance</a></li>
+        <li><a href="">Training</a></li>
+        <li><a href="">Early Management</a></li>
+        <li><a href="">Quality Maintenance</a></li>
+        <li><a href="">Administrative and Support</a></li>
+        <li><a href="">Safety and Environmental</a></li>
+    </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>OC Denver TPM wiki home</title>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+  </head>
+  <body id="wikiHome">
+    <?php include 'includes/leftMenu.php'; ?>
+    <div id="mainContent">
+      <iframe src="pages/introduction.html"></iframe>
+    </div>
+  </body>
+</html>
